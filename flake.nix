56fe5e513fd6b0b8b5865626002b7fec0cec4727
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-08-01 12:58:53 -0300

    Initialize Nix flake with a devShell for golang

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Experiment: Open a non-blocker PDF previewer in go";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ go gopls gotools go-tools ];
+        };
+      });
+}
