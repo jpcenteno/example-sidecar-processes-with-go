3516bd64c7d58bb96f06d53ac017beca477f03c1
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2024-08-04 20:41:08 -0300

    Remove dead code

--- main.go
+++ main.go
@@ -15,27 +15,6 @@ type PdfPreviewProcess struct {
 	cmd *exec.Cmd
 }
 
-// OpenPdfPreview starts a zathura process with the given file path
-func OpenPdfPreview(filePath string) (*PdfPreviewProcess, error) {
-	// Preliminary checks
-	if strings.ToLower(filepath.Ext(filePath)) != ".pdf" {
-		return nil, fmt.Errorf("provided file is not a PDF: %s", filePath)
-	}
-
-	if _, err := exec.LookPath("zathura"); err != nil {
-		return nil, fmt.Errorf("Command Zathura not found")
-	}
-
-	// Open Zathura
-	cmd := exec.Command("zathura", filePath)
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true} // Give the process it's own group.
-	if err := cmd.Start(); err != nil {
-		return nil, fmt.Errorf("failed to start zathura: %v", err)
-	}
-
-	return &PdfPreviewProcess{cmd: cmd}, nil
-}
-
 // Close kills the zathura process
 func (ppp *PdfPreviewProcess) Close() error {
 	return syscall.Kill(-ppp.cmd.Process.Pid, syscall.SIGKILL) // Negative sign sends signal to whole process group.
